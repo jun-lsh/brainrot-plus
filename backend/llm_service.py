a4f98775ca4cfd5c5442913c629a2f11a0a3fa32
poipiii <nanosonico1@gmail.com>
2024-01-20 15:57:39 +0800

    added poetry and llm service

---
+++ backend/llm_service.py
@@ -0,0 +1,21 @@
+from openai import AzureOpenAI
+from config import prompt_template
+
+def construct_prompt(context:str) -> str:
+    return prompt_template.format(context=context)
+
+def generate(prompt:str,model_instance:str,client:AzureOpenAI) -> str:
+    try:
+        completion = client.chat.completions.create(
+            model=model_instance,  # e.g. gpt-35-instant
+            messages=[
+                {
+                    "role": "user",
+                    "content": construct_prompt(prompt),
+                },
+            ],
+        )
+        return completion.choices[0].message.content
+
+    except Exception as e:
+        print(e)
